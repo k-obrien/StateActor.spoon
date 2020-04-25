5b70656a0153a2cd36f366a9a9ad4b60f5647c01
Kieran O'Brien <developer@obrien.network>
2020-04-26 00:22:41 +1000

    Initial commit.

---
+++ README.md
@@ -0,0 +1,20 @@
+# State Actor
+[![License](https://img.shields.io/badge/License-GPL%203.0-blue.svg)](https://www.gnu.org/licenses/gpl-3.0.html)
+
+System power/session event listener for macOS powered by Hammerspoon's caffeinate.watcher API.
+
+## LICENSE
+	Copyright (C) 2020-present Kieran O'Brien
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
---
+++ init.lua
@@ -0,0 +1,67 @@
+--- === StateActor ===
+---
+--- Assign functions to be called when the system enters a new state.
+---
+--- Download: [https://github.com/k-obrien/state-actor/raw/master/StateActor.spoon.zip](https://github.com/k-obrien/state-actor/raw/master/StateActor.spoon.zip)
+
+local obj={}
+obj.__index = obj
+
+-- Metadata
+obj.name = "StateActor"
+obj.version = "0.1"
+obj.author = "Kieran O'Brien"
+obj.homepage = "https://github.com/k-obrien/state-actor"
+obj.license = "GPLv3 - https://opensource.org/licenses/GPL-3.0"
+
+--- StateActor:bindActions(mapping)
+--- Method
+--- Binds actions for StateActor
+---
+--- Parameters:
+---  * mapping - A table containing tables of functions to call when entering one of the below states:
+---   * screensaverDidStart - The screensaver started
+---   * screensaverDidStop - The screensaver stopped
+---   * screensaverWillStop - The screensaver is about to stop
+---   * screensDidLock - The screen was locked
+---   * screensDidSleep - The displays have gone to sleep
+---   * screensDidUnlock - The screen was unlocked
+---   * screensDidWake - The displays have woken from sleep
+---   * sessionDidBecomeActive - The session became active, due to fast user switching
+---   * sessionDidResignActive - The session is no longer active, due to fast user switching
+---   * systemDidWake - The system woke from sleep
+---   * systemWillPowerOff - The user requested a logout or shutdown
+---   * systemWillSleep - The system is preparing to sleep
+function obj:bindActions(mapping)
+   local state = hs.caffeinate.watcher
+   self.states = {}
+   self.states[state.screensaverDidStart] = mapping.screensaverDidStart
+   self.states[state.screensaverDidStop] = mapping.screensaverDidStop
+   self.states[state.screensaverWillStop] = mapping.screensaverWillStop
+   self.states[state.screensDidLock] = mapping.screensDidLock
+   self.states[state.screensDidSleep] = mapping.screensDidSleep
+   self.states[state.screensDidUnlock] = mapping.screensDidUnlock
+   self.states[state.screensDidWake] = mapping.screensDidWake
+   self.states[state.sessionDidBecomeActive] = mapping.sessionDidBecomeActive
+   self.states[state.sessionDidResignActive] = mapping.sessionDidResignActive
+   self.states[state.systemDidWake] = mapping.systemDidWake
+   self.states[state.systemWillPowerOff] = mapping.systemWillPowerOff
+   self.states[state.systemWillSleep] = mapping.systemWillSleep
+end
+
+--- StateActor:start()
+--- Method
+--- Start StateActor
+---
+--- Parameters:
+---  * None
+function obj:start()
+   self.watcher = hs.caffeinate.watcher.new(self.onSystemState):start()
+   return self
+end
+
+function obj.onSystemState(state)
+   for _, action in pairs(obj.states[state]) do action() end
+end
+
+return obj
